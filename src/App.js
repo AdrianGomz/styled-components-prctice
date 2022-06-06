0a80bb9386c29e889a79bac10a109562fc86ce88
isaac <vi13150@innovaccion.mx>
2022-06-05 22:50:48 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { Formik, Form } from "formik";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import { useState } from "react";
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`;
 
+const Section = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 500px;
+  box-shadow: 0px 2px 3px rgb(0, 0, 0, 0.3);
+`;
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit;
+  for (let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1);
+  }
+  return Math.round(total);
+};
+const Balance = styled.span`
+  font-size: 36px;
+  margin-top: 12px;
+  display: inline-block;
+  color: black;
+`;
 function App() {
+  const [balance, setBalance] = useState("");
+  const handleSubmit = ({ deposit, contribution, years, rate }) => {
+    const val = compoundInterest(
+      Number(deposit),
+      Number(contribution),
+      Number(years),
+      Number(rate)
+    );
+    setBalance(formatter.format(val));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: "",
+            contribution: "",
+            years: "",
+            rate: "",
+          }}
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form>
+            <Input name="deposit" label="deposito inicial" />
+            <Input name="contribution" label="Contribucion anual" />
+            <Input name="years" label="Años" />
+            <Input name="rate" label="Interes estimado" />
+            <Button>Caular</Button>
+          </Form>
+        </Formik>
+        {balance !== "" ? <Balance>Balance final: {balance}</Balance> : null}
+      </Section>
+    </Container>
   );
 }
 
